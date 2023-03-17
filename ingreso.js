cf03c38be0e7c6d65d311e4d930b574a8d5c01d3
Andres Garzon <115560952+AndresGarzonCastillo@users.noreply.github.com>
2023-03-16 20:49:36 -0500

    Add files via upload

---
+++ ingreso.js
@@ -0,0 +1,16 @@
+const edad = parseInt(prompt("¿Cuál es tu edad?"));
+const promesaIngreso = new Promise ((resolve, reject) => {
+  if (edad >= 18) {
+    resolve ("Eres mayor de edad, puedes ingresar");
+} else if (edad <= 17){
+    reject ("Eres menor de edad, no puedes ingresar");
+  }
+  else if (edad != Number.NaN){
+    reject ("El documento presentado no es valido");
+}
+});
+promesaIngreso.then((valor) => {
+  console.log(`started ${valor}`);
+}).catch((error)=>{
+  console.log(error);
+});
